9027093dfb5ff3a53ebe1c651e27fee2eb7cd176
pinchao <cpc4253@163.com>
2023-08-20 18:55:11 +0800

    add measure buid

---
+++ build/measure.ts
@@ -0,0 +1,22 @@
+import SpeedMeasurePlugin from 'speed-measure-webpack-plugin'
+
+const isMEASURE = process.env.MEASURE
+
+// handle MiniCssExtractPlugin bug in SpeedMeasurePlugin: https://github.com/stephencookdev/speed-measure-webpack-plugin/issues/167
+const handleConfigWithMiniCssExtractPlugin = (config) => {
+  const cssPluginIndex = config.plugins.findIndex(
+    (e) => e.constructor.name === 'MiniCssExtractPlugin'
+  )
+  const cssPlugin = config.plugins[cssPluginIndex]
+  const configToExport = new SpeedMeasurePlugin().wrap(config)
+  configToExport.plugins[cssPluginIndex] = cssPlugin
+  return configToExport
+}
+
+const measureConfig = (config) => {
+  const configToExport = handleConfigWithMiniCssExtractPlugin(config)
+
+  return isMEASURE ? configToExport : config
+}
+
+export default measureConfig
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,9 +3,13 @@ import commonConfig from './build/webpack.base.config'
 import productionConfig from './build/webpack.prod.config'
 import developmentConfig from './build/webpack.dev.config'
 import { WebpackOptionsNormalized, Configuration } from 'webpack'
+import measureConfig from './build/measure'
 
 export default (env): Partial<WebpackOptionsNormalized> | Configuration => {
   const isProduction: boolean = env && env.WEBPACK_BUILD
+
   const config = isProduction ? productionConfig : developmentConfig
-  return merge(commonConfig, config)
+  const mergeConfig = merge(commonConfig, config)
+
+  return measureConfig(mergeConfig)
 }
