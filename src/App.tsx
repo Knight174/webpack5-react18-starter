d1efe8421263c4ab3d2f88aa72881be8416b12ec
pinchao <cpc4253@163.com>
2023-08-20 17:19:06 +0800

    add image config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react'
 import './style/App.scss'
+import img from 'src/assets/wechat.png'
 
 export default function App() {
   console.log('App re-render')
 
   useEffect(() => {
-    // 副作用 hook 并没有依赖任何的状态数据，所以只会执行一次；而下面的那些依赖了某些状态，因此只要状态发生变化，hook 就会执行。
     console.log('mounted App 挂载（只一次）')
   }, [])
 
@@ -14,6 +14,7 @@ export default function App() {
       {/* Content */}
       <main className="wrapper">
         <div>Hello world!</div>
+        <img width={'300px'} src={img} alt="wechat" />
       </main>
     </div>
   )
